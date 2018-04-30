81bbbdd06abf22c15bdd189a6e932a2916011c7b
Vinay Kumar <vinay.kumar@nagarro.com>
2018-04-30 16:19:13 +0530

    Add the functionality to delete book , book instance and genre

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -91,13 +91,26 @@ exports.bookinstance_create_post = [
 ];
 
 // Display BookInstance delete form on GET.
-exports.bookinstance_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete GET');
+exports.bookinstance_delete_get = function(req, res, next) {
+    BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec(function(err,bookinstance){
+        if(err){
+            return next(err);
+        }
+
+        res.render('bookinstance_delete',{title : 'Delete Book Instance', bookinstance : bookinstance});
+    });
 };
 
 // Handle BookInstance delete on POST.
-exports.bookinstance_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete POST');
+exports.bookinstance_delete_post = function(req, res, next) {
+    // Assume valid BookInstance id in field.
+    BookInstance.findByIdAndRemove(req.body.id, function deleteBookInstance(err) {
+        if (err) { return next(err); }
+        // Success, so redirect to list of BookInstance items.
+        res.redirect('/catalog/bookinstances');
+        });
 };
 
 // Display BookInstance update form on GET.
